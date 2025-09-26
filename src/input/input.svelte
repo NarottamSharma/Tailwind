79809784872feebda2675a7a2cb3b2c9909faf45
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-26 13:22:01 +0530

    Enhance input component layout for improved styling and consistency

--- src/input/input.svelte
+++ src/input/input.svelte
@@ -4,9 +4,10 @@
   const errorId = error ? `${id}-error` : undefined;
 </script>
 
-<div>
-  <label for={id}>{label}</label>
+<div class="space-y-1">
+  <label class="block font-medium text-slate-900  " for={id}>{label}</label>
   <input
+  class="block bg-white w-full outline outline-slate-600 rounded px-2 py-1 focus:outline-2 focus:outline-blue-400 focus:bg-blue-100 focus:text-neutral-900 placeholder-slate-700 placeholder:italic border outline-offset-4 user-invalid:outline-red-600"
     {id}
     aria-describedby={descriptionId}
     aria-invalid={!!error}
