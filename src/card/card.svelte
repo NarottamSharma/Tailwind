8e3f97486efc46c8fb742a5c022735376ef7b75a
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-26 12:40:45 +0530

    Enhance card component styling for better visual consistency

--- src/card/card.svelte
+++ src/card/card.svelte
@@ -1,8 +1,9 @@
 <script>
   const { title = 'Card Title', description = '' } = $props();
+  
 </script>
 
-<div>
-  <h2>{title}</h2>
-  <p>{description}</p>
+<div class="border-2 p-4 rounded-md bg-slate-50 border-slate-500" >
+  <h2 class="font-semibold text-lg   " >{title}</h2>
+  <p class="mt-1   "  >{description}</p>
 </div>
