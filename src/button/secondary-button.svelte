f24ee0b8177f51c129173217b538d43705f3827e
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-26 12:14:13 +0530

    Refactor button components for improved readability and consistency

--- src/button/secondary-button.svelte
+++ src/button/secondary-button.svelte
@@ -2,4 +2,6 @@
   const { label = 'Button', ...props } = $props();
 </script>
 
-<button {...props}>{label}</button>
+<button
+  class="-inset offset -8 rounded-md bg-white px-3 py-1.5 shadow-sm ring-2 outline-4 outline-slate-600 ring-inset hover:bg-slate-100 hover:outline-3"
+  {...props}>{label}</button>
